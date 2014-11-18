d99d9a79d6ffb8bc8b77c8dfcb873d68317247e3
Alvaro Garcia <maxpowel@gmail.com>
2014-11-18 17:51:13 +0100

    Fixed issue with new php versions

--- mpdf.php
+++ mpdf.php
@@ -18585,8 +18585,9 @@ function CloseTag($tag)
 		unset($this->InlineAnnots[$tag]);	// *ANNOTATIONS*
 	}
 	else { 
-		if (isset($this->InlineProperties[$tag]) && count($this->InlineProperties[$tag])) { 
-			$this->restoreInlineProperties(array_pop($this->InlineProperties[$tag])); 
+		if (isset($this->InlineProperties[$tag]) && count($this->InlineProperties[$tag])) {
+            $tmpProps = array_pop($this->InlineProperties[$tag]);
+            $this->restoreInlineProperties($tmpProps);
 		}
 		if (isset($this->InlineAnnots[$tag]) && count($this->InlineAnnots[$tag])) { 	// *ANNOTATIONS*
 			$annot = array_pop($this->InlineAnnots[$tag]); 	// *ANNOTATIONS*
